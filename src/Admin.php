049e9e365da36c3f13400eb182aa25cd800273d8
Fabian Marz <fabian@netzstrategen.com>
2017-06-28 12:38:33 +0200

    WIP - Logical and readbility code refactoring.

--- src/Admin.php
+++ src/Admin.php
@@ -17,14 +17,14 @@ class Admin {
    * @implements init
    */
   public static function init() {
-    add_action('admin_menu', __CLASS__ . '::menu');
+    add_action('admin_menu', __CLASS__ . '::admin_menu');
     add_action('admin_init', __CLASS__ . '::admin_init');
   }
 
   /**
    * @implements admin_menu
    */
-  public static function menu() {
+  public static function admin_menu() {
     add_menu_page(__('Shop Analytics Settings', Plugin::L10N), __('Shop Analytics', Plugin::L10N), 'manage_options', 'shop-analytics', [__CLASS__, 'renderSettings'], 'dashicons-chart-area');
   }
 
@@ -32,15 +32,15 @@ class Admin {
    * @implements admin_init
    */
   public static function admin_init() {
-    register_setting('shop-analytics-settings', 'shop_analytics_gtm_container');
-    register_setting('shop-analytics-settings', 'shop_analytics_gtm_container_embed');
+    register_setting('shop-analytics-settings', 'shop_analytics_gtm_id');
+    register_setting('shop-analytics-settings', 'shop_analytics_gtm_enabled');
     register_setting('shop-analytics-settings', 'shop_analytics_track_user_id');
     register_setting('shop-analytics-settings', 'shop_analytics_track_user_role');
     register_setting('shop-analytics-settings', 'shop_analytics_disable_user_tracking');
   }
 
   /**
-   * @implements renderSettings
+   * @see static::admin_menu()
    */
   public static function renderSettings() {
     Plugin::renderTemplate(['templates/settings.php']);
