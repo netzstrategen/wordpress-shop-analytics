779638a39809fd9f326eccd68c1002610a27210e
Joan López <joan@netzstrategen.com>
2018-05-22 09:28:55 +0200

    Fixed Google Tag Manager ID prefix was duplicated when embeding non-JS version container.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Analytics
-  Version: 1.0.3
+  Version: 1.0.4
   Text Domain: shop-analytics
   Description: Integrates Google Tag Manager and Google Analytics into WooCommerce.
   Author: netzstrategen
