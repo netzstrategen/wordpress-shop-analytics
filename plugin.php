8396aa69ea32985ab30739151187f86bee2ca7c1
Joan López <joan@netzstrategen.com>
2019-04-30 15:42:38 +0200

    fixup! Fixed orders where pushed more that once to Google dataLayer.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Analytics
-  Version: 1.5.3
+  Version: 1.5.4
   Text Domain: shop-analytics
   Description: Integrates Google Tag Manager and Google Analytics into WooCommerce.
   Author: netzstrategen
