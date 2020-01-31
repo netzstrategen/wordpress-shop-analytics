27387f9601ae0e40301c0eb970a30edc14410039
Andreas Blumberg <andreas@netzstrategen.com>
2020-01-31 09:13:54 +0100

    Changed event_data to array.

--- assets/scripts/datalayer/common.js
+++ assets/scripts/datalayer/common.js
@@ -115,8 +115,9 @@ document.shopAnalytics = {
       }
       if (shop_analytics_settings.tc_enabled == 1) {
         window.tc_vars = window.tc_vars || [];
-        //window.tC.event.generic_event = window.tC.event.generic_event || [];
-        window.tC.event.generic_event(this, {"payload": event_data });
+        window.event_data = window.event_data || [];
+        window.event_data.push(event_data);
+        window.tC.event.generic_event(this, {"payload": window.event_data });
       }
       else {
         window.dataLayer.push(event_data);
