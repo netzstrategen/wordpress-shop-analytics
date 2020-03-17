5c7ac09e382f69ac7663c9d77142325e9425498d
Luca Pipolo <luca@netzstrategen.com>
2020-03-17 10:46:26 +0100

    Updated npm packages and fix gulpfile to use gulp4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,67 @@
-var autoprefixer = require('gulp-autoprefixer');
-var eol = require('gulp-eol');
-var eslint = require('gulp-eslint');
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var uglify = require('gulp-uglify');
+const eol = require('gulp-eol');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
 
-gulp.task('eslint', function () {
-  return gulp.src(['assets/scripts/**/*.js', '!node_modules/**', '!gulpfile.js'])
+// Source and dist folders.
+const srcScripts = 'assets/scripts/**/*.js';
+const distScripts = './dist/scripts';
+
+/**
+ * Lints JavaScript files.
+ */
+gulp.task('eslint', function() {
+  const task = gulp.src([
+    srcScripts,
+    '!node_modules/**',
+    '!gulpfile.js'
+  ])
     .pipe(eslint())
     .pipe(eslint.format());
+  return task;
 });
 
-gulp.task('scripts', ['eslint'], function () {
-  gulp.src('assets/scripts/**/*.js')
-  .pipe(uglify({
-    output: { beautify: false }
-  }))
-  .on('error', function(e){
-    console.log(e);
-  })
-  .pipe(eol('\n'))
-  .pipe(gulp.dest('./dist/scripts'));
+/**
+ * Parse, minify and beautify JavaScript files.
+ */
+gulp.task('scripts', function() {
+  const task = gulp.src(srcScripts)
+    .pipe(uglify({
+      output: { beautify: false }
+    }))
+    .on('error', function(e){
+      console.log(e);
+    })
+    .pipe(eol('\n'))
+    .pipe(gulp.dest(distScripts));
+  return task;
 });
 
-gulp.task('build', function(callback) {
-  runSequence('scripts', callback);
+/**
+ * Defines the build Gulp task.
+ */
+gulp.task('build', gulp.series(
+  'eslint',
+  'scripts',
+), function(cb) {
+  cb;
 });
 
+/**
+ * Watches files and run related tasks if there are changes.
+ */
 gulp.task('watch', function () {
-  gulp.watch('assets/scripts/**/*.js',['scripts']);
+  const task = gulp.watch(srcScripts, gulp.series(
+    'eslint', 'scripts'
+  ));
+  return task;
 });
 
-gulp.task('default', function () {
-  gulp.start('build');
+/**
+ * Defines the default Gulp task.
+ */
+gulp.task('default', gulp.series(
+  'build'
+), function(cb) {
+  cb;
 });
