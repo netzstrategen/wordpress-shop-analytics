567f63b2f9443a07a5de0714dcebabd987caf702
Callum <callum@netzstrategen.com>
2021-01-13 16:01:28 +0000

    Added Google Optimise snippet.

--- src/Admin.php
+++ src/Admin.php
@@ -31,6 +31,7 @@ class Admin {
     register_setting('shop-analytics-settings', 'shop_analytics_track_user_role');
     register_setting('shop-analytics-settings', 'shop_analytics_disable_user_tracking');
     register_setting('shop-analytics-settings', 'shop_analytics_track_ecommerce');
+    register_setting('shop-analytics-settings', 'shop_analytics_google_optimize');
     register_setting('shop-analytics-settings', 'shop_analytics_market_default');
     register_setting('shop-analytics-settings', 'shop_analytics_datalayer_logging');
     register_setting('shop-analytics-settings', 'shop_analytics_brand_attribute');
