96be82125d8dcbc6441bf6b4697eb938037e11d6
Joan López <joan@somoscuatro.es>
2022-05-17 17:01:48 +0200

    Fixed warning when retrieving current user role.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Analytics
-  Version: 1.11.5
+  Version: 1.11.6
   Text Domain: shop-analytics
   Description: Integrates Google Tag Manager and Google Analytics into WooCommerce.
   Author: netzstrategen
