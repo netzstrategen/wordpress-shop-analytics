6c1199878a9a4c2d1c4ec16b48cb74cdc6614fbb
Daniel Kudwien <daniel@netzstrategen.com>
2022-07-19 19:57:00 +0200

    bump version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Shop Analytics
-  Version: 1.11.7
+  Version: 1.12.0
   Text Domain: shop-analytics
   Description: Integrates Google Tag Manager and Google Analytics into WooCommerce.
   Author: netzstrategen
